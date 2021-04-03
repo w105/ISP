086e94494f24deb352df68b5205299ac2df4ab97
w105 <79201235+w105@users.noreply.github.com>
2021-04-03 10:27:57 +0300

    Add files via upload

---
+++ Lab 3/Lab 3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Lab_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            
+            n = Convert.ToInt32(Console.ReadLine());
+            
+            BB Bumaga4 = new BB(n);
+
+            for(int i=0;i<n;i++)
+            {
+                Bumaga4[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for(int i=0;i<n;i++)Console.WriteLine(Bumaga4[i]);
+
+        }
+    }
+}
